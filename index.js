07fb9a629104c03db8fd7b6dc039d7091e8b4f5d
Oscar Mesa <becario.oscar.mesa@agileinnova.org>
2021-01-15 07:38:54 -0500

    make it better

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('dev'))
+    
+app.get('/', (req, res) => {
+    res.send("<h1>API Academia</h1>")
+})
+
+app.get('/estudiantes', (req, res) => {
+    let estudiantes = []
+    estudiantes.push({nombre: 'pepito',apellido : 'perez', edad : 20})
+    res.json(estudiantes)
+})
+    
+app.listen(8083, () => {
+    console.log("Aplicación corriendo en el puerto 8083!!")
+})
